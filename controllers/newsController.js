5de41016d0b59b4e70bfa9d02c8acfe7b9eb4256
utso1707097 <utso097.csekuet@gmail.com>
2024-04-22 22:29:15 -0700

    Added news store api

---
+++ controllers/newsController.js
@@ -0,0 +1,76 @@
+import prisma from "../DB/db.config.js";
+import { generateRandomNum, imageValidator } from "../utils/helper.js";
+import { newsSchema } from "../validations/newsValidation.js";
+import vine, { errors } from "@vinejs/vine";
+
+class NewsController {
+  static async index(req, res) {}
+
+  static async store(req, res) {
+    try {
+      const user = req.user;
+      const body = req.body;
+      const validator = vine.compile(newsSchema);
+      const payload = await validator.validate(body);
+
+      if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({
+          errors: {
+            image: "Image field is required",
+          },
+        });
+      }
+      const image = req.files?.image;
+      // Image custom validator
+      const message = imageValidator(image?.size, image?.mimetype);
+      if (message !== null) {
+        return res.status(400).json({
+          errors: {
+            image: message,
+          },
+        });
+      }
+
+      //Image upload
+      const imgExt = image?.name.split(".");
+      const imageName = generateRandomNum() + "." + imgExt[1];
+      const uploadPath = process.cwd() + "/public/images/news/" + imageName;
+
+      image.mv(uploadPath, (err) => {
+        if (err) throw err;
+      });
+
+      payload.image = imageName;
+      payload.user_id = user.id;
+
+      const news = await prisma.news.create({
+        data: payload,
+      });
+
+      return res.json({
+        status: 200,
+        message: "News created successfully!",
+        news,
+      });
+    } catch (error) {
+      if (error instanceof errors.E_VALIDATION_ERROR) {
+        // array created by SimpleErrorReporter
+        // console.log(error.messages)
+        return res.status(400).json({ errors: error.messages });
+      } else {
+        return res.status(500).json({
+          status: 500,
+          message: " Something went wrong. Please try again later.",
+        });
+      }
+    }
+  }
+
+  static async show(req, res) {}
+
+  static async update(req, res) {}
+
+  static async destroy(req, res) {}
+}
+
+export default NewsController;
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import AuthController from "../controllers/authController.js";
 import authMiddleware from "../middleware/authenticate.js";
 import ProfileController from "../controllers/profileController.js";
+import NewsController from "../controllers/newsController.js";
 
 const router = Router();
 
@@ -11,4 +12,12 @@ router.post("/auth/login", AuthController.login);
 // Profile routes
 router.get("/profile", authMiddleware, ProfileController.index); //Private route
 router.put("/profile/:id", authMiddleware, ProfileController.update); //Private route
+
+// News routes
+router.get("/news", NewsController.index);
+router.post("/news", authMiddleware, NewsController.store);
+router.get("/news/:id", NewsController.show);
+router.put("/news/:id", NewsController.update);
+router.delete("/news/:id", NewsController.destroy);
+
 export default router;
